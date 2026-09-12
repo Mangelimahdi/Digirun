824d6fb5e5a22da45910872380e04f3655516972
mahdimangeli1381 <mahdimangeligl@gmail.com>
2026-09-12 21:21:34 +0330

    feat: implement phone authentication flow

--- src/features/auth/layout/AuthLayout.jsx
+++ src/features/auth/layout/AuthLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router'
 const AuthLayout = () => {
     return (
         <div className='flex justify-center items-center h-screen px-2'>
-                <Outlet />
+            <Outlet />
         </div>
     )
 }
