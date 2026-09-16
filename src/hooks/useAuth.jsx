6c7cb3f27189f2fff08350fa4cb9123a61a56911
mahdimangeli1381 <mahdimangeligl@gmail.com>
2026-09-16 13:43:14 +0330

    fix: import useEffect in useAuth

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import useCountDown from './useCountDown';
 import { useNavigate } from 'react-router';
 import { validate } from '../Validator';
